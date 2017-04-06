614666c80a2772cc830c4c3441687dcf229ceab7
Jichao Ouyang <oyanglulu@gmail.com>
2017-04-07 00:25:40 +0800

    embedded type in expr and injector

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const AlaCarte = require('./output/AlaCarte')
-const {
-    In,
-  Inl,
-  Inr,
-  interpretExpr,
-  interpreterOr,
-  Interpreter,
-  inject,
-  functorOr,
-} = AlaCarte
+const {In,
+       Inl,
+       Inr,
+       interpretExpr,
+       interpreterOr,
+       Interpreter,
+       inject,
+       functorOr,
+      } = AlaCarte
 const {Functor} = require('./output/Data.Functor')
 function interpreterFor(functor, interpreter) {
   return new Interpreter(function () {
@@ -27,31 +26,60 @@ function fromArray(array, functor) {
 }
 
 function injectInto(typeArray, type) {
-  let head = typeArray[0]
-  let tail = typeArray.slice(1)
-  if(head == type && tail.length == 0 ){
+
+  if(!typeArray.type){
     return AlaCarte.injectId(type)
-  }else if(head == type){
-    return AlaCarte.injectLeft(head)(fromArray(tail, functorOr))
-  }else if(tail.length != 0){
-    return AlaCarte.injectRight(head)(fromArray(tail, functorOr))(type)(injectInto(tail, type))
+  }else if(typeArray.type.head == type){
+    return AlaCarte.injectLeft(typeArray.type.head)(typeArray.type.tail)
+  }else if(typeArray.type.tail){
+    return AlaCarte.injectRight(typeArray.type.head)(typeArray.type.tail)(type)(injectInto(typeArray.type.tail, type))
   }
   throw new Error(`Failed inject type into typeArray`);
 }
 
 function injectorFrom(types) {
-  return t => injectInto(types, t)
+  let functorTypes = fromArray(types, functorOrT)
+  function injector(t){
+    let inject = injectInto(functorTypes, t)
+    inject.type = {sub: t, sup: functorTypes}
+    return inject
+  }
+  injector.type = functorTypes
+  return injector
 }
 
 function interpreterFrom(types) {
   return fromArray(types, interpreterOr)
 }
 
+function functorOrT(head) {
+  return function(tail) {
+    let functor = functorOr(head)(tail)
+    functor.type = {head, tail}
+    return functor
+  }
+}
+
+function injectT(injector) {
+  return function (adt) {
+    let expr = inject(injector)(adt);
+    expr.injector = injector
+
+    return expr
+    }
+}
+
+function supTypeSameAs(injector){
+  return function(expr){
+    return expr.injector.type.sup == injector.type
+  }
+}
 module.exports = {
   injectorFrom,
   interpreterFrom,
   interpreterFor,
   interpretExpr,
   Functor,
-  inject,
+  inject: injectT,
+  supTypeSameAs,
 }
