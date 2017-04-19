e591b386a7082a9d1d50c38faaa83e521a9ab24b
Jichao Ouyang <oyanglulu@gmail.com>
2017-04-19 12:58:52 +0800

    compare each type in array

--- __tests__/index.js
+++ __tests__/index.js
@@ -2,73 +2,115 @@ const {
   injectorFrom,
   interpreterFrom,
   interpretExpr,
-  Functor,
   interpreterFor,
   inject,
+  isInjectedBy,
   Val,
   Expr,
-} = require('../index.es6')
+} = require('../index.es6');
 
-const {Add, Mult} = Expr.create({Add: ['value0', 'value1'], Mult: ['value0', 'value1']})
-const evalAdd = interpreterFor(Add, function (v) {
-  return v.value0 + v.value1
+const { Add, Mult } = Expr.create({
+  Add: ['value0', 'value1'],
+  Mult: ['value0', 'value1'],
 });
 
-const evalVal = interpreterFor(Val, function (v) {
-  return v.value
+const evalAdd = interpreterFor(Add, function(v) {
+  return v.value0 + v.value1;
 });
 
-const evalMult = interpreterFor(Mult, function (v) {
-  return v.value0 * v.value1 | 0;
+const evalVal = interpreterFor(Val, function(v) {
+  return v.value;
+});
+
+const evalMult = interpreterFor(Mult, function(v) {
+  return (v.value0 * v.value1) | 0;
 });
 
 test('adds 1 + 2 to equal 3', () => {
-  let interpreter = interpreterFrom([evalVal, evalAdd])
-  let injector = injectorFrom([Val, Add])
-  let [val, add] = injector.inject()
-  let expr = add(val(1), val(2))
+  let interpreter = interpreterFrom([evalVal, evalAdd]);
+  let injector = injectorFrom([Val, Add]);
+  let [val, add] = injector.inject();
+  let expr = add(val(1), val(2));
   expect(interpretExpr(interpreter)(expr)).toBe(3);
 });
 
 test('(mult (add 1 3) 4) to equal 16', () => {
   // a la carte new data type Mult
-  let interpreter = interpreterFrom([evalVal, evalAdd, evalMult])
-  let injector = injectorFrom([Val, Add, Mult])
-  let [val, add, mult] = injector.inject()
-  let expr = mult(add(val(1), val(3)), val(4))
+  let interpreter = interpreterFrom([evalVal, evalAdd, evalMult]);
+  let injector = injectorFrom([Val, Add, Mult]);
+  let [val, add, mult] = injector.inject();
+  let expr = mult(add(val(1), val(3)), val(4));
   expect(interpretExpr(interpreter)(expr)).toBe(16);
-})
+});
 
 test('user interpreter as printer', () => {
-  const printMult = interpreterFor(Mult, v => `(${v.value0} * ${v.value1})`)
-  const printAdd = interpreterFor(Add, v => `(${v.value0} + ${v.value1})`)
-  const printVal = interpreterFor(Val, v => `${v.value}`)
-  const printer = interpreterFrom([printVal, printAdd, printMult])
-  let injector = injectorFrom([Val, Add, Mult])
-  let [val, add, mult] = injector.inject()
-  let expr = mult(add(val(1), val(3)), val(4))
-  expect(interpretExpr(printer)(expr)).toBe('((1 + 3) * 4)')
-})
+  const printMult = interpreterFor(Mult, v => `(${v.value0} * ${v.value1})`);
+  const printAdd = interpreterFor(Add, v => `(${v.value0} + ${v.value1})`);
+  const printVal = interpreterFor(Val, v => `${v.value}`);
+  const printer = interpreterFrom([printVal, printAdd, printMult]);
+  let injector = injectorFrom([Val, Add, Mult]);
+  let [val, add, mult] = injector.inject();
+  let expr = mult(add(val(1), val(3)), val(4));
+  expect(interpretExpr(printer)(expr)).toBe('((1 + 3) * 4)');
+});
 
 test('arbitrary type', () => {
-  let interpreter = interpreterFrom([evalVal, evalAdd])
-  let injector = injectorFrom([Val, Add])
-  let [val, add] = injector.inject()
-  let expr = add(val('1'), val('2'))
-  expect(interpretExpr(interpreter)(expr)).toBe("12");
-})
+  let interpreter = interpreterFrom([evalVal, evalAdd]);
+  let injector = injectorFrom([Val, Add]);
+  let [val, add] = injector.inject();
+  let expr = add(val('1'), val('2'));
+  expect(interpretExpr(interpreter)(expr)).toBe('12');
+});
 
 test('order does not matter, but injector and interpreter has to be the same', () => {
-  let interpreter = interpreterFrom([evalAdd, evalVal, evalMult])
-  let injector = injectorFrom([Add, Val, Mult])
-  let [add, val, mult] = injector.inject()
-  let expr = mult(add(val(1), val(3)), val(4))
+  let interpreter = interpreterFrom([evalAdd, evalVal, evalMult]);
+  let injector = injectorFrom([Add, Val, Mult]);
+  let [add, val, mult] = injector.inject();
+  let expr = mult(add(val(1), val(3)), val(4));
   expect(interpretExpr(interpreter)(expr)).toBe(16);
-})
+});
 test('missing argument', () => {
-  let interpreter = interpreterFrom([evalAdd, evalVal, evalMult])
-  let injector = injectorFrom([Add, Val, Mult])
-  let [add, val, mult] = injector.inject()
-  let expr = mult(add(val(1), val(3)), val(4))
+  let interpreter = interpreterFrom([evalAdd, evalVal, evalMult]);
+  let injector = injectorFrom([Add, Val, Mult]);
+  let [add, val, mult] = injector.inject();
+  let expr = mult(add(val(1), val(3)), val(4));
   expect(interpretExpr(interpreter)(expr)).toBe(16);
-})
+});
+
+test('isInjectedBy same order of Expr Types', () => {
+  let injector = injectorFrom([Add, Val, Mult]);
+  let [add, val, mult] = injector.inject();
+  let expr = mult(add(val(1), val(3)), val(4));
+  expect(isInjectedBy(injector)(expr)).toBe(true);
+
+  let AnotherInjector = injectorFrom([Add, Val, Mult]); //same order and type
+  let [anotherAdd, anotherVal, anotherMult] = AnotherInjector.inject();
+  let anotherExpr = anotherMult(anotherVal(3), anotherVal(4));
+  expect(isInjectedBy(injector)(anotherExpr)).toBe(true);
+
+  let lessInjector = injectorFrom([Val, Mult]); //same order and type
+  let [lessAdd, lessVal] = lessInjector.inject();
+  let lessExpr = lessAdd(lessVal(3), lessVal(4));
+  expect(isInjectedBy(injector)(lessExpr)).toBe(false);
+
+  let wrongOrderIjector = injectorFrom([Add, Mult, Val]); //wrong order
+  let [
+    wrongOrderAdd,
+    wrongOrderVal,
+    wrongOrderMult,
+  ] = wrongOrderIjector.inject();
+  let wrongOrderInjectedExpr = wrongOrderMult(
+    wrongOrderVal(3),
+    wrongOrderVal(4)
+  );
+
+  expect(isInjectedBy(injector)(wrongOrderInjectedExpr)).toBe(false);
+
+  let another = Expr.create({
+    Add: ['value0', 'value1'],
+  });
+  let diffTypeInjector = injectorFrom([another.Add, Val, Mult]); //wrong type Add
+  let [diffTypeAdd, diffTypeVal, diffTypeMult] = diffTypeInjector.inject();
+  let diffTypeExpr = diffTypeAdd(diffTypeVal(1), diffTypeVal(3));
+  expect(isInjectedBy(injector)(diffTypeExpr)).toBe(false);
+});
--- index.es6
+++ index.es6
@@ -1,77 +1,91 @@
-const {In,
-       Inl,
-       Inr,
-       interpretExpr,
-       interpreterOr,
-       Interpreter,
-       inject,
-       injectId,
-       injectLeft,
-       injectRight,
-       functorOr,
-       Functor,
-      } = require('./alacarte')
+const {
+  In,
+  Inl,
+  Inr,
+  interpretExpr,
+  interpreterOr,
+  Interpreter,
+  inject,
+  injectId,
+  injectLeft,
+  injectRight,
+  functorOr,
+  Functor,
+} = require('./alacarte');
 
 function interpreterFor(expr, interpreter) {
-  return new Interpreter(function () {
+  return new Interpreter(function() {
     return expr.__TYPE__;
   }, interpreter);
 }
 
 function injectorFrom(exprs) {
-  let supTypes = _functorFromArray(functorOrT)(exprs.map(expr=>expr.__TYPE__))
-  function injector(type){
-    let inject = _injectInto(supTypes, type)
-    inject.__TYPE__ = {__SUB__: type, __SUP__: supTypes}
-    return inject
+  let typesList = exprs.map(expr => expr.__TYPE__);
+  let supTypes = _functorFromArray(functorOrT)(typesList);
+  function injector(type) {
+    let inject = _injectInto(supTypes, type);
+    inject.__TYPE__ = { __SUB__: type, __SUP__: typesList };
+    return inject;
   }
-  return {__TYPE__: supTypes,
-          inject: function(){
-            return exprs.map(x=>x.inject(injector))
-          }}
+  return {
+    __TYPE__: typesList,
+    inject: function() {
+      return exprs.map(x => x.inject(injector));
+    },
+  };
 }
 
 function functorOrT(__LEFT__) {
   return function(__RIGHT__) {
-    let functor = functorOr(__LEFT__)(__RIGHT__)
-    functor.__TYPE__ = {__LEFT__, __RIGHT__}
-    return functor
-  }
+    let functor = functorOr(__LEFT__)(__RIGHT__);
+    functor.__TYPE__ = { __LEFT__, __RIGHT__ };
+    return functor;
+  };
 }
 
 function injectT(injector) {
-  return function (adt) {
+  return function(adt) {
     let expr = inject(injector)(adt);
-    expr.injector = injector
-    return expr
-  }
+    expr.injector = injector;
+    return expr;
+  };
 }
 
-function supTypeSameAs(injector){
-  return function(expr){
-    return expr.injector.__TYPE__.__SUP__ == injector.__TYPE__
-  }
+function supTypeSameAs(injector) {
+  return function(expr) {
+    let types = injector.__TYPE__;
+    let supTypes = expr.injector.__TYPE__.__SUP__;
+    if (types.length != supTypes.length) return false;
+    for (let index = 0; index < supTypes.length; index++) {
+      if (supTypes[index] != types[index]) return false;
+    }
+    return true;
+  };
 }
 
 function _functorFromArray(functor) {
-  return function go(array){
-    let head = array[0]
-    let tail = array.slice(1)
-    if(tail.length===0) {
+  return function go(array) {
+    let head = array[0];
+    let tail = array.slice(1);
+    if (tail.length === 0) {
       return head;
-    }else{
-      return functor(head)(go(tail))
+    } else {
+      return functor(head)(go(tail));
     }
-  }
+  };
 }
 
 function _injectInto(functorOr, type) {
-  if(!functorOr.__TYPE__){
-    return injectId(type)
-  }else if(functorOr.__TYPE__.__LEFT__ == type){
-    return injectLeft(functorOr.__TYPE__.__LEFT__)(functorOr.__TYPE__.__RIGHT__)
-  }else if(functorOr.__TYPE__.__RIGHT__){
-    return injectRight(functorOr.__TYPE__.__LEFT__)(functorOr.__TYPE__.__RIGHT__)(type)(_injectInto(functorOr.__TYPE__.__RIGHT__, type))
+  if (!functorOr.__TYPE__) {
+    return injectId(type);
+  } else if (functorOr.__TYPE__.__LEFT__ == type) {
+    return injectLeft(functorOr.__TYPE__.__LEFT__)(
+      functorOr.__TYPE__.__RIGHT__
+    );
+  } else if (functorOr.__TYPE__.__RIGHT__) {
+    return injectRight(functorOr.__TYPE__.__LEFT__)(
+      functorOr.__TYPE__.__RIGHT__
+    )(type)(_injectInto(functorOr.__TYPE__.__RIGHT__, type));
   }
   throw new Error(`Failed inject type into functorOr`);
 }
@@ -79,40 +93,48 @@ function _injectInto(functorOr, type) {
 function Expr(name, args) {
   this.name = name;
   this.args = args;
-  this.__TYPE__ = new Functor(f => v => Object.create({}, argsToMap(v,this.args,f)))
+  this.__TYPE__ = new Functor(f => v =>
+    Object.create({}, argsToMap(v, this.args, f)));
 }
 
-function argsToMap(v, args, f=(_=>_)) {
-  return args.map((arg,index) => ({[arg]: {value: f(v[arg], index),  enumerable: true}})).reduce((acc, a)=>Object.assign(acc, a), {})
+function argsToMap(v, args, f = _ => _) {
+  return args
+    .map((arg, index) => ({
+      [arg]: { value: f(v[arg], index), enumerable: true },
+    }))
+    .reduce((acc, a) => Object.assign(acc, a), {});
 }
-Expr.prototype.inject = function(exprSupport){
-  return (...a) => inject(exprSupport(this.__TYPE__))(Object.create({}, argsToMap({}, this.args, (_, index)=>a[index])))
-}
+Expr.prototype.inject = function(exprSupport) {
+  return (...a) =>
+    injectT(exprSupport(this.__TYPE__))(
+      Object.create({}, argsToMap({}, this.args, (_, index) => a[index]))
+    );
+};
 Expr.create = function(desc) {
-  var result = {}
+  var result = {};
   for (var expr in desc) {
     if (desc.hasOwnProperty(expr)) {
-      result[expr] = new Expr(expr, desc[expr])
+      result[expr] = new Expr(expr, desc[expr]);
     }
   }
-  return result
-}
+  return result;
+};
 
 function _Val() {
   this.args = ['value'];
-  this.__TYPE__ = new Functor(f => v => Object.create({}, argsToMap(v,this.args)))
+  this.__TYPE__ = new Functor(f => v =>
+    Object.create({}, argsToMap(v, this.args)));
 }
-_Val.prototype = Expr.prototype
+_Val.prototype = Expr.prototype;
 
 module.exports = {
   injectorFrom,
   interpreterFrom: _functorFromArray(interpreterOr),
   interpreterFor,
   interpretExpr,
-  Functor,
   inject: injectT,
   supTypeSameAs,
   isInjectedBy: supTypeSameAs,
   Expr,
-  Val: new _Val,
-}
+  Val: new _Val(),
+};
